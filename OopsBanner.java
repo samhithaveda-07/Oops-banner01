c22cd685c2835509d506982b8adf3daafc3b0ea6
samhithaveda-07 <samhithaveda2507@gmail.com>
2026-03-10 00:07:11 +0530

    Create OopsBanner.java

---
+++ OopsBanner.java
@@ -0,0 +1,20 @@
+public class OopsBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("************************************************************");
+        System.out.println("*                                                          *");
+        System.out.println("*   ******    ******   ******    ******                   *");
+        System.out.println("*  *      *  *      *  *      *  *      *                  *");
+        System.out.println("*  *      *  *      *  *      *  *      *                  *");
+        System.out.println("*  *      *  *      *  ******    ******                   *");
+        System.out.println("*  *      *  *      *  *         *                        *");
+        System.out.println("*  *      *  *      *  *         *                        *");
+        System.out.println("*   ******    ******   *         *                        *");
+        System.out.println("*                                                          *");
+        System.out.println("*                 OBJECT ORIENTED PROGRAMMING              *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+
+    }
+}
